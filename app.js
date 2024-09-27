5471e869b350161a999474907f3fe34ce97a5103
André Luiz <andre.luiz.a.f@hotmail.com>
2024-09-26 21:19:25 -0300

    atualizando app.js

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const storedUser = localStorage.getItem("loggedUser");
+  if (storedUser) {
+    const userData = JSON.parse(storedUser);
+    displayUserCard(userData);
+    toggleLoginform(false);
+  }
+});
 
-function toggleLoginform(show){
-    document.getElementById('login-form').style.display = show ? 'block' : 'none';
-    document.getElementById('user-card').style.display = show ? 'none' : 'block';
+function displayUserCard(user) {
+  const { firstName, lastName, age, email, phone, image } = user;
+  document.getElementById("user-image").src = image;
+  document.getElementById("user-name").textContent = `${firstName} ${lastName}`;
+  document.getElementById("user-age").textContent = age;
+  document.getElementById("user-email").textContent = email;
+  document.getElementById("user-phone").textContent = phone;
 }
 
-document.getElementById('login-form').addEventListener('submit', async function (e) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+function toggleLoginform(show) {
+  document.getElementById("login-form").style.display = show ? "block" : "none";
+  document.getElementById("user-card").style.display = show ? "none" : "block";
+}
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
     try {
-
+      const loginResponse = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      if (!loginResponse.ok) {
+        const userData = await loginResponse.JSON();
+        localStorage.setItem("loggedUser", JSON.stringify(userData));
+      } else {
+        alert("Login falhou! verifique suas credenciais.");
+        toggleLoginform(false);
+        document.getElementById("user-card").style.display = "block";
+      }
     } catch (error) {
-
+      console.error("Error", error);
+      alert("Invalid username or password");
     }
-    
-}
-
-);
+  });
