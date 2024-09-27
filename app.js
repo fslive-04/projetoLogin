c306cde231455eea7a5ec29afb0ea9ee0664ff56
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-09-26 21:51:31 -0300

    fix: JSON to json

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const storedUser = localStorage.getItem('loggedUser');
-  if (storedUser) {
-    const userData = JSON.parse(storedUser);
-    displayUserCard(userData);
-    toggleLoginform(false);
-  }
+    const storedUser = localStorage.getItem('loggedUser');
+    if (storedUser) {
+        const userData = JSON.parse(storedUser);
+        displayUserCard(userData);
+        toggleLoginform(false);
+    }
 });
 
 function displayUserCard(user) {
-  const { firstName, lastName, age, email, phone, image } = user;
-  document.getElementById('user-image').src = image;
-  document.getElementById('user-name').textContent = `${firstName} ${lastName}`;
-  document.getElementById('user-email').textContent = email;
-  document.getElementById('user-age').textContent = age;
-  document.getElementById('user-phone').textContent = phone;
+    const { firstName, lastName, age, email, phone, image } = user;
+    document.getElementById('user-image').src = image;
+    document.getElementById('user-name').textContent = `${firstName} ${lastName}`;
+    document.getElementById('user-email').textContent = email;
+    document.getElementById('user-age').textContent = age;
+    document.getElementById('user-phone').textContent = phone;
 }
 
 function toggleLoginform(show) {
-  document.getElementById("login-form").style.display = show ? "block" : "none";
-  document.getElementById("user-card").style.display = show ? "none" : "block";
+    document.getElementById('login-form').style.display = show ? 'block' : 'none';
+    document.getElementById('user-card').style.display = show ? 'none' : 'block';
 }
 
-document
-  .getElementById("login-form")
-  .addEventListener("submit", async function (e) {
+document.getElementById('login-form').addEventListener('submit', async function (e) {
     e.preventDefault(); //Evita o envio padrão do form
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
     try {
-      const loginResponse = await fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username,
-          password
-        }),
-      });
+        const loginResponse = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
 
-      if (loginResponse.ok) {
-        const userData = await loginResponse.JSON();
-        localStorage.setItem("loggedUser", JSON.stringify(userData));
-        displayUserCard(userData);
-        toggleLoginform(false);
-      } else {
-        alert("Login falhou! Verifque suas credenciais.");
-      }
-      
+        if (loginResponse.ok) {
+            const userData = await loginResponse.json();
+            localStorage.setItem('loggedUser', JSON.stringify(userData));
+            displayUserCard(userData);
+            toggleLoginform(false);
+            clearLoginForm();
+        } else {
+            alert('Login falhou! Verifque suas credenciais.');
+        }
+
     } catch (error) {
-      //console.error("Error", error);
-      //alert("Erro na conexão");
+        console.error('Erro:', error);
+        clearLoginForm();
     }
-  });
+});
 
-  function cleanLoginForm() {
-
-  }
+function clearLoginForm() {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+}
